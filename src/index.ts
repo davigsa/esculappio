022c06f779432db93a338f2a2a4a351b97aa43db
Davi Sá <davi.goulartsa@gmail.com>
2021-10-11 16:19:52 -0300

    Avoiding some errors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import helmet from "helmet";
 
 import { usersRoutes } from './users/user.routes'
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -18,6 +20,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRoutes)
+
+// Threating Errors
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
